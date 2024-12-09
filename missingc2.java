06beecfe73df8786d45d60feb2449cfc845ecc8f
jeev86399 <nnani86399@gmail.com>
2024-12-09 14:49:21 +0530

    Add files via upload

---
+++ missingc2.java
@@ -0,0 +1,20 @@
+import java.util.*;
+public class missingc2{
+    public static void main(String args[]){
+        Scanner s=new Scanner(System.in);
+        int n=s.nextInt();
+        int arr[]=new int[n];
+        for(int i=0;i<n;i++)
+        arr[i]=s.nextInt();
+        Arrays.sort(arr);
+        int max=arr[n-1];
+        int c[]=new int[max+1];
+        System.out.println("missing number in count array:")
+;        for(int i=0;i<n;i++)
+            c[arr[i]]++;
+        for(int i=0;i<max;i++){
+            if (c[i]==0)
+            System.out.println(i+" ");
+        }
+    }
+}
